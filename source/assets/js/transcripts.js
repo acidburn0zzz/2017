83e70512f0e457cb284deee5be28a7a95a840c1d
Laura Kalbag <me@laurakalbag.com>
2015-03-02 12:54:56 +0000

    Got the transcripts with select box working

--- source/assets/js/transcripts.js
+++ source/assets/js/transcripts.js
@@ -49,7 +49,7 @@
 			// add select to the page
 			list.parentNode.insertBefore(select, list);
 			// add linking functionality to select
-			select.setAttribute('onchange','window.location.href=this.value');
+			// select.setAttribute('onchange','window.location.href=this.value');
 			// remove list from page (as select has replaced functionality)
 			list.parentNode.removeChild(list);
 		});
@@ -64,14 +64,44 @@
 			transcript.setAttribute('aria-hidden', 'true');
 			transcript.classList.add('hidden');
 			transcriptOption.setAttribute('aria-expanded', 'false');
+			transcriptOption.removeAttribute('selected');
 		});
+	}
+
+	function findURLIDandMatchingTranscript() {
+		// get the URL's ID
+		var urlID = window.location.hash;
+		// console.log("The URL ID is: " + urlID);
+
+		// if the URL has an ID, get it
+		if (urlID === "") {
+			// console.log("There's no ID")
+			visibleTranscript = document.getElementById('transcript-en');
+		} else {
+			// console.log("There's an ID")
+			// get the first character in the string to the end of the string (everything)
+			var urlIDWithoutTheHash = urlID.substr(1);
+
+			// check if there's a transcript with the same ID
+			var matchingTranscript = document.getElementById(urlIDWithoutTheHash);
+			matchingTranscriptId = matchingTranscript.id;
+			// console.log("The matching transcript is: " + matchingTranscriptId);
+			visibleTranscript = document.getElementById(matchingTranscriptId);
+		}
+	}
+
+	function chooseVisibleTranscript() {
+		// if there's an #internal-link in the URL which corresponds with a transcript ID, then set that as the visible transcript
+		findURLIDandMatchingTranscript();
+		// if there's no #internal-link in the URL, just go with the English transcript
+		// (without the var, it's a global variable, rather than restricted to this function)
 	}
 
 	// fold up transcripts so only the English one is visible
 	function hideOtherTranscripts() {
 		hideAllTranscripts();
 		// choose the transcript that is going to be shown (English by default, this can be adjusted later)
-		var visibleTranscript = document.getElementById('transcript-en');
+		chooseVisibleTranscript();
 		// console.log(visibleTranscript)
 		visibleTranscript.setAttribute('aria-hidden', 'false');
 		visibleTranscript.classList.remove('hidden');
@@ -80,6 +110,8 @@
 		visibleTranscriptOption = document.querySelector('[data-transcript-id=' + visibleTranscriptID + ']');
 		// set aria expanded true on select menu option
 		visibleTranscriptOption.setAttribute('aria-expanded', 'true');
+		// set selected state to select menu option
+		visibleTranscriptOption.setAttribute('selected', 'selected');
 	}
 
 	// Set the state of the transcript.
@@ -89,38 +121,38 @@
 
 		if (shouldShow) {
 			// Show the transcript.
-			console.log("Showing transcript: " + transcript.id)
+			// console.log("Showing transcript: " + transcript.id)
 			hideAllTranscripts();
 			transcript.classList.remove('hidden', 'transcript-collapsed');
 			transcript.setAttribute('aria-hidden', 'false');
 
 			// set aria expanded true on select menu option
 			transcriptOption.setAttribute('aria-expanded', 'true');
+			// set selected state to select menu option
+			transcriptOption.setAttribute('selected', 'selected');
 		} else {
 			// Hide the transcript.
-			console.log("Hiding transcript: " + transcript.id)
+			// console.log("Hiding transcript: " + transcript.id)
 			transcript.classList.add('hidden', 'transcript-collapsed');
 			transcript.setAttribute('aria-hidden', 'true');
 
 			// set aria expanded false on select menu option
 			transcriptOption.setAttribute('aria-expanded', 'false');
+			// remove the selected attribute
+			transcriptOption.removeAttribute('selected');
 		}
 	}
 
 	// switch the visible state of the transcripts
 	function switchTranscript(transcriptSelect) {
 		var selectedOption = transcriptSelect.getAttribute('data-transcript-id');
-		console.log("Selected option:" + selectedOption);
+		// console.log("Selected option:" + selectedOption);
 		var selectedTranscript = document.querySelector('#' + selectedOption);
-		console.log("Selected transcript:" + selectedTranscript);
-
-		// if the selected selectedTranscript isn't hidden, don't do anything
-		// if the selectedTranscript isn't the selected selectedTranscript, hide it
-		// if the selected selectedTranscript is hidden, show it
+		// console.log("Selected transcript:" + selectedTranscript);
 
 		// if the transcript has the hidden class
 		var isHidden = (selectedTranscript.className.match('hidden') === 'true');
-		console.log("Is it hidden?" + isHidden)
+		// console.log("Is it hidden?" + isHidden)
 
 		// run the transcript through the show/hide function
 		show(selectedTranscript, !isHidden);
@@ -129,19 +161,31 @@
 	}
 
 	// when the relevant transcript is selected from the dropdown, show that transcript text
-	function addTranscriptMenuClickHandlers() {
-        Array.prototype.forEach.call(document.querySelector('#transcript-select'), function (transcriptSelect) {
-            transcriptSelect.addEventListener('click', function (event) {
-                // console.log('a click!');
-                console.log(transcriptSelect);
-                switchTranscript(event.currentTarget);
-            });
-        });
-    }
+	function addTranscriptMenuChangeHandlers() {
+
+		var transcriptSelect = document.getElementById('transcript-select')
+
+		// console.log('Adding to: ' + transcriptSelect);
+
+		transcriptSelect.addEventListener('change', function (event) {
+			var selectionBox = event.currentTarget
+
+			// Update the URL
+			var selectionBoxValue = selectionBox.value
+			window.location.href=selectionBoxValue
+
+			// Update URL and matching transcript
+			findURLIDandMatchingTranscript();
+
+			// Handle the transcript switch.
+			var selectedOption = selectionBox.options[selectionBox.selectedIndex]
+			switchTranscript(selectedOption);
+		}, true);
+	}
 
 	// when one transcript is shown, hide the others
 
 	transcriptListIntoDropdown();
 	hideOtherTranscripts();
-	addTranscriptMenuClickHandlers();
+	addTranscriptMenuChangeHandlers();
 })();
